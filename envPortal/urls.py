76b883a732462f169a321c719dd9a1c32fcbbb63
Xcelled <j12@ketelaar.com>
2016-04-15 16:56:13 -0400

    Fixed home templating

--- envPortal/urls.py
+++ envPortal/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
 from . import views
 
-app_name = 'envportal'
+#app_name = 'envportal'
 
 urlpatterns = [
-	url(r'^home/$', views.home)
+	url(r'^home/$', views.home, name='home'),
+	url(r'^calendar/$', views.calendar, name='calendar'),
+	url(r'^create_event/$', views.create_event, name='create_event'),
+	url(r'^event_more_info/$', views.create_event, name='event_more_info'),
+	url(r'^find_resources/$', views.find_resources, name='find_resources'),
+	url(r'^log_in/$', views.log_in, name='log_in'),
+	url(r'^sign_up/$', views.sign_up, name='sign_up'),
 ]
--- envPortal/views.py
+++ envPortal/views.py
@@ -4,3 +4,27 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'envportal/home.html')
 #enddef
+
+def calendar(request):
+    return render(request, 'envportal/calendar.html')
+#enddef
+
+def create_event(request):
+    return render(request, 'envportal/create_event.html')
+#enddef
+
+def event_more_info(request):
+    return render(request, 'envportal/event_more_info.html')
+#enddef
+
+def find_resources(request):
+    return render(request, 'envportal/find_resources.html')
+#enddef
+
+def log_in(request):
+    return render(request, 'envportal/log_in.html')
+#enddef
+
+def sign_up(request):
+    return render(request, 'envportal/sign_up.html')
+#enddef
