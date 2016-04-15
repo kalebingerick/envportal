84d6870940bd0104eabe98e4eeee5fa24055d4ee
John Sellers <jrs9025@rit.edu>
2016-04-15 18:42:00 -0400

    this might actually fix it

--- uxexperience/settings.py
+++ uxexperience/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+STATIC_ROOT= os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
