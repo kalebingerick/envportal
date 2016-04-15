44b2b7696a44344c8ca541aa94e01c0a319f9bd3
John Sellers <jrs9025@rit.edu>
2016-04-15 12:42:17 -0400

    web fixes continue

--- source/views.py
+++ source/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 import os
-from source import templates
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, '../source/templates/home.html')
