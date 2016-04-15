c81c1d08b88c035f9c6efaa397faf7c9fa79ad8a
John Sellers <jrs9025@rit.edu>
2016-04-15 13:11:09 -0400

    not even sure anymore

--- envTest/views.py
+++ envTest/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 import os
-from .models import Greeting
 # Create your views here.
 def home(request):
-    return render(request, 'home.html',{'greetings': greetings} )
+    return render(request, 'home.html' )
