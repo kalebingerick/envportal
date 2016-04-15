2e5ccdb60190513037a6037061447dff2a3f0a79
John Sellers <jrs9025@rit.edu>
2016-04-15 12:51:56 -0400

    creating a django app

--- envPortal/urls.py
+++ envPortal/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-import source.views
+import envTest.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
