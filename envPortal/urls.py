c7c15867e205826101eb8f7cd075fb2ddd508f5f
John Sellers <jrs9025@rit.edu>
2016-04-15 02:39:02 -0400

    import work

--- envPortal/urls.py
+++ envPortal/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from ../site import views
+from EnvPortal.site import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
