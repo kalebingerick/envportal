40c93c7142ac06b7415e97b1bd359b7ea54b8ebe
John Sellers <jrs9025@rit.edu>
2016-04-17 14:25:57 -0400

    Made template fixes, and tested them locally should work, looks like there might be some addressing issues with log_in but the html would just need to change planning on merging this into master after this commit

--- envPortal/views.py
+++ envPortal/views.py
@@ -10,9 +10,28 @@ def calendar(request):
 #enddef
 
 def create_event(request):
-    return render(request, 'envportal/create_event.html')
+	if request.user.is_authenticated():
+		return render(request, 'envportal/create_event.html')
+	else:
+		need_auth(request)
+		return render(request, 'envportal/create_event.html')
 #enddef
 
+def edit_event(request):
+	if request.user.is_authenticated():
+		return render(request, 'envportal/edit_events.html')
+	else:
+		need_auth(request)
+		return render(request, 'envportal/edit_events.html')
+
+def need_auth(request):
+	log_in(request)
+	return request
+#enddef
+
+def user_dashboard(request):
+	return render(request, 'envportal/user_dashboard.html')
+	
 def event_more_info(request):
     return render(request, 'envportal/event_more_info.html')
 #enddef
