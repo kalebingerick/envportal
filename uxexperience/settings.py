5b5d945fc028e6c9a38f698499bc177f4cd47078
Xcelled <j12@ketelaar.com>
2016-04-17 17:17:55 -0400

    Added some heroku guards

--- uxexperience/settings.py
+++ uxexperience/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -106,8 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Heroku DB configuration
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+try: import dj_database_url
+except ImportError: pass
+else:
+	db_from_env = dj_database_url.config(conn_max_age=500)
+	DATABASES['default'].update(db_from_env)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
--- uxexperience/wsgi.py
+++ uxexperience/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uxexperience.settings")
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+try: from whitenoise.django import DjangoWhiteNoise
+except ImportError: application = get_wsgi_application()
+else: application = DjangoWhiteNoise(application)
