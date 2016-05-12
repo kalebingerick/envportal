564978186e57094292f9da3e4f8e4b600fb161fa
Kaleb Davis <kalebdavisgithub@gmail.com>
2016-05-11 23:25:55 -0400

    import settings

--- uxexperience/urls.py
+++ uxexperience/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
+from django.conf import settings
 from envPortal import views
 
 urlpatterns = [
